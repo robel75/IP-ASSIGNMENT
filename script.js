24c376841dce640e8c4d245bace202de10f902e7
Amanuel <amanuelbehailu557@gmail.com>
2026-01-10 22:08:27 +0300

    navbar added for ancient and the navbar put into comments in css

---
+++ script.js
@@ -0,0 +1,21 @@
+const toggleButton = document.getElementById('toggle-btn')
+const sidebar = document.getElementById('sidebar')
+
+function toggleSubMenu(button){
+  button.nextElementSibling.classList.toggle('show')
+  button.classList.toggle('rotate')
+  if (sidebar.classList.contains('close')){
+    sidebar.classList.toggle('close')
+    toggleButton.classList.toggle('rotate')
+  }
+}
+
+function toggleSidebar(){
+    sidebar.classList.toggle('close')
+    toggleButton.classList.toggle('rotate')
+
+    Array.from(sidebar.getElementsByClassName('show')).forEach(ul => {
+        ul.classList.remove('show')
+        ul.previousElementSibling.classList.remove('rotate')
+    })
+}
