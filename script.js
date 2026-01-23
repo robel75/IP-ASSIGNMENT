9c9b2a0ac4282763f56b261193484e68cd40d0de
Amanuel <amanuelbehailu557@gmail.com>
2026-01-23 09:30:37 +0300

    js comments

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ function toggleSubMenu(button){
   button.classList.toggle('rotate') //submenu expand button rotates
   if (sidebar.classList.contains('close')){ //fixes the issue of the elements going out of the sidebar when sidebar is closed
     sidebar.classList.toggle('close')    //this removes close if sidebar contains close, which means it expands
-    toggleButton.classList.toggle('rotate') //rotates the icon,
+    toggleButton.classList.toggle('rotate') //rotates the icon
   }
 }
 
 function toggleSidebar(){
     sidebar.classList.toggle('close') //closes sidebar if it was open or opens it if it was closed
-    toggleButton.classList.toggle('rotate')
+    toggleButton.classList.toggle('rotate') //rotates the button
 
     Array.from(sidebar.getElementsByClassName('show')).forEach(ul => {
         ul.classList.remove('show')
