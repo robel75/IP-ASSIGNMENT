f6be7f96b57a2c410b334beef054075dfcd56f25
Amanuel <amanuelbehailu557@gmail.com>
2026-01-23 09:30:11 +0300

    js comments

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ const sidebar = document.getElementById('sidebar')
 function toggleSubMenu(button){
   button.nextElementSibling.classList.toggle('show') //submenu shows
   button.classList.toggle('rotate') //submenu expand button rotates
-  if (sidebar.classList.contains('close')){ //fixes the issue of the elements
-    sidebar.classList.toggle('close')       //going out of the sidebar when sidebar is closed
-    toggleButton.classList.toggle('rotate')
+  if (sidebar.classList.contains('close')){ //fixes the issue of the elements going out of the sidebar when sidebar is closed
+    sidebar.classList.toggle('close')    //this removes close if sidebar contains close, which means it expands
+    toggleButton.classList.toggle('rotate') //rotates the icon,
   }
 }
 
 function toggleSidebar(){
-    sidebar.classList.toggle('close')
+    sidebar.classList.toggle('close') //closes sidebar if it was open or opens it if it was closed
     toggleButton.classList.toggle('rotate')
 
     Array.from(sidebar.getElementsByClassName('show')).forEach(ul => {
