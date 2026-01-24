bd1e0fd3d2d650f9717c99f9933845c0767bb899
Amanuel <amanuelbehailu557@gmail.com>
2026-01-24 08:32:20 +0300

    final for now

--- script.js
+++ script.js
@@ -2,20 +2,20 @@ const toggleButton = document.getElementById('toggle-btn')
 const sidebar = document.getElementById('sidebar')
 
 function toggleSubMenu(button){
-  button.nextElementSibling.classList.toggle('show') //submenu shows
-  button.classList.toggle('rotate') //submenu expand button rotates
-  if (sidebar.classList.contains('close')){ //fixes the issue of the elements going out of the sidebar when sidebar is closed
-    sidebar.classList.toggle('close')    //this removes close if sidebar contains close, which means it expands
-    toggleButton.classList.toggle('rotate') //rotates the icon
+  button.nextElementSibling.classList.toggle('show')
+  button.classList.toggle('rotate')
+  if (sidebar.classList.contains('close')){
+    sidebar.classList.toggle('close')
+    toggleButton.classList.toggle('rotate')
   }
 }
 
 function toggleSidebar(){
-    sidebar.classList.toggle('close') //closes sidebar if it was open or opens it if it was closed
-    toggleButton.classList.toggle('rotate') //rotates the button
-     //converts them to array so we can use forEach
-    Array.from(sidebar.getElementsByClassName('show')).forEach(ul => { //finds all submenus that are currently open inside the sidebar
-        ul.classList.remove('show') //close the submenu
-        ul.previousElementSibling.classList.remove('rotate') //the button that was rotated when opened is back to default
+    sidebar.classList.toggle('close')
+    toggleButton.classList.toggle('rotate')
+
+    Array.from(sidebar.getElementsByClassName('show')).forEach(ul => {
+        ul.classList.remove('show')
+        ul.previousElementSibling.classList.remove('rotate')
     })
 }
